9b807efec400066e1f11e7914fd612a6fabc57c4
Evan L Gilbert <Evan.Gilbert1212@gmail.com>
2020-04-24 13:45:38 -0400

    Added Time of Day To User Summary

--- ViewModels/UserSummary.cs
+++ ViewModels/UserSummary.cs
@@ -5,6 +5,7 @@ namespace UpsideAPI.ViewModels
   public class UserSummary
   {
     public string FirstName { get; set; }
+    public string TimeOfDay { get; set; }
     public DateTime PeriodBeginDate { get; set; }
     public DateTime PeriodEndDate { get; set; }
     public double AccountBalance { get; set; }
